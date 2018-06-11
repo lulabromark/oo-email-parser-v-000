191e26d9d3595780df1a42aa8d163cfe0eead108
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-11 00:18:05 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   end
 
   def parse
-    @emails.split(/\s|[, ]/).reject! { |string| string.empty? }.uniq!
-    emails
+    array = @emails.split(/\s|[, ]/).reject! { |string| string.empty? }.uniq!
+    array
   end
 end
